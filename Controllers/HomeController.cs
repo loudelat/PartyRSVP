5ab737e6dc2bc78639313bf61e3beebef10b6910
Unknown <loudelat@amazon.com>
2018-06-04 14:26:34 -0700

    Added ViewResult Index method Created RSVP.cshtml page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,9 +10,9 @@ namespace PartyRSVP.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            return View("RSVP");
         }
 
         public IActionResult About()
